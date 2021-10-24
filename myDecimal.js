e1db81e2d2a44f9db5d1d97bfa5d01a57ec7acd4
Drogon <deadreebook@gmail.com>
2021-10-24 15:31:15 +0700

    added myDecimal

---
+++ myDecimal.js
@@ -0,0 +1,13 @@
+var ourDecimal = 5.7;
+
+// Only change code below this line
+
+var myDecimal = 6.5;
+
+// Multiply Two Decimals
+
+var product = 2.0 * 2.5; // 5
+
+// Divide One Decimal by Another
+
+var quotient = 4.4 / 2.0; // 2.2
