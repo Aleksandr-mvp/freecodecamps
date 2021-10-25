d731eca3c2492c75a539db6ad3cd161e7187171e
Drogon <deadreebook@gmail.com>
2021-10-25 21:45:40 +0700

    added tasks

---
+++ concatenatingStrings.js
@@ -0,0 +1,9 @@
+var myStr = "This is the start. " + "This is the end."; // Change this line
+
+// Only change code below this line
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
+// Only change code below this line
+var myName = "Aleksandr";
+var myStr = "My name is " + myName + "and I am well!";
