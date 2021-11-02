affb4fd698019c804c6eaf7d934e581b8dcac3c5
Drogon <deadreebook@gmail.com>
2021-11-02 19:11:28 +0700

    added tasks

---
+++ Shopping List.js
@@ -0,0 +1,7 @@
+const myList = [
+    ["Chocolate Bar", 15],
+    ["Containing several", 16],
+    ["Number representing", 17],
+    ["Element should", 18],
+    ["number representing", 18, [1, 2, 3]]
+  ];
