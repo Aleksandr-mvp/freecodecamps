00b55ea4f0912f9e577d8ace8ad6c0a06f431022
Drogon <deadreebook@gmail.com>
2021-11-08 21:02:15 +0700

    added tasks

---
+++ Build JavaScript Objects.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    name: "frank",
+    legs: 4,
+    tails: 1,
+    friends: ["human", "Dogs",]
+
+    // Only change code above this line
+};
